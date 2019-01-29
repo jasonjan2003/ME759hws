18e016b100a0a4196a893a11c14031c0594efcdd
jasonjan2003 <jasonjan2003@gmail.com>
2019-01-28 21:30:40 -0600

    problem1 slurm shell

---
+++ HW1/problem1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -p queue_name
+#SBATCH --job-name=HW1-1
+#SBATCH -N 1 -n 1 --gres=gpu:1	# 1 node
+#SBATCH -o job_out
+#SBATCH -e job_err
+
+cd $SLURM_SUBMIT_DIR
+./problem1
+
+mv job_out problem1.o$SLURM_JOB_ID
+mv job_err problem1.e$SLURM_JOB_ID
