ec9f178d20e13932e8f19fc173d0364cad1694df
jasonjan2003 <jasonjan2003@gmail.com>
2019-01-31 23:24:59 -0600

    problem 1a

---
+++ HW2/problem1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	
+	int i;						// current value read from file
+	FILE *fp;					// the file
+
+	// open the file
+	fp = fopen("./problem1.in","r");
+
+	// Check if file opened okay
+	if( fp == NULL ){
+		perror("error opening file.");
+		return(-1);
+	}
+
+	fscanf (fp, "%d", &i);		// first line contains number of numbers in list
+	printf("%d\n", i);			// task 1: print out # of integers read in
+	int arr[i];					// declare int array with that many elements
+
+	// read in rest of list
+	for (int a = 0; a<i; a++)
+	{
+		fscanf(fp, "%d\n", arr[a]);
+		printf("%d\n", arr[a]);
+	}
+	fclose(fp);
+
+
+	return 0;
+}
