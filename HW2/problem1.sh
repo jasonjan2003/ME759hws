5fd78cd594391678281fae98caf03327c81456c0
jasonjan2003 <jasonjan2003@gmail.com>
2019-02-03 12:48:29 -0600

    HW2,problem1 sbatch script

---
+++ HW2/problem1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -p slurm_shortgpu
+#SBATCH --job-name=HW2-1
+#SBATCH -N 1 -n 1 --gres=gpu:1	# 1 node
+#SBATCH -o job_out
+#SBATCH -e job_err
+
+cd $SLURM_SUBMIT_DIR
+for i in {10..19}
+do
+	printf "2^%i:\n";
+	./test_file_gen1 $i;
+	./problem1;
+	printf "\n";
+done
+
+mv job_out problem1.o$SLURM_JOB_ID
+mv job_err problem1.e$SLURM_JOB_ID
